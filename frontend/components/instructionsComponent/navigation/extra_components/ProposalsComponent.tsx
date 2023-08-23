ceffc1ba76473248d7a339ca7d8f1e6eb452139d
Ghk21nznz21 <goncalo1999almeida@gmail.com>
2023-08-23 13:43:22 +0100

    finallyyyyyyyyyyyyyyyyyyyyyy

--- frontend/components/instructionsComponent/navigation/extra_components/ProposalsComponent.tsx
+++ frontend/components/instructionsComponent/navigation/extra_components/ProposalsComponent.tsx
@@ -12,42 +12,31 @@ function generateDivWithData(name: string, value: any) {
 }
 
 function Proposals() {
-  // track html answer and when its prepared 
   const [jsxElements, setJsxElements] = useState<JSX.Element[]>([]);
   const [done, setDone] = useState(false);
-
-  // track proposals
   const [proposalsList, setProposalsList] = useState<any[]>([]);
-
-  // track index 
   const [index, setIndex] = useState<number>(0);
-
-  // track end of loop
   const [isLoading, setIsLoading] = useState(true);
 
+  const { data, isError, isLoading: contractIsLoading } = useContractRead({
+    address: '0x14d3f34208c82A3458f82Dd7CdBe5CE2bd9B39ce',
+    abi: Ballot.abi,
+    functionName: 'proposals',
+    args: [index],
+  }) as { data: [string, number]; isError: boolean; isLoading: boolean };
 
   useEffect(() => {
-    if (!isLoading) {
-      type ProposalData = [string, number];
-      const fetchData = async () => {
-        const { data, isError, isLoading } = await useContractRead({
-          address: '0x14d3f34208c82A3458f82Dd7CdBe5CE2bd9B39ce',
-          abi: Ballot.abi,
-          functionName: 'proposals',
-          args: [index],
-        }) as { data: ProposalData; isError: boolean; isLoading: boolean };
-        console.log(data, isError, isLoading);
-        if (isError) setIsLoading(false);
-        if (data) {
-          // when to jump to next proposal request 
-          setProposalsList((prevList) => [...prevList, data]);
-          setIndex((prevIndex) => prevIndex + 1);
-        }
-      };
-
-      fetchData();
+    if (contractIsLoading) {
+      return; // Do nothing while the contract data is loading
     }
-  }, [index, isLoading]);
+
+    if (isError) {
+      setIsLoading(false);
+    } else if (data) {
+      setProposalsList((prevList) => [...prevList, data]);
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  }, [data, isError, contractIsLoading]);
 
   useEffect(() => {
     if (!isLoading && proposalsList.length > 0) {
@@ -59,9 +48,6 @@ function Proposals() {
   return (
     <div>
       {!done ? <p>Loading... </p> : jsxElements}
-      {index}
-      {isLoading}
-      {proposalsList}
     </div>
   );
 }
